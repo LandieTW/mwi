acc8b09f5166eb0844d1bfd5a5d599badfd97bc4
Daniel Teixeira Wanderley <dteixeirawanderley@gmail.com>
2025-09-27 15:16:53 -0300

    it works

--- bot_trade/start.py
+++ bot_trade/start.py
@@ -1,11 +1,20 @@
 """
 Descritpion:
     - This is a sample code to start trading with Binance Spot API using the binance-sdk-spot package.
+    - Python connector - https://github.com/binance/binance-connector-python
 
 Necessary
     - Python (version 3.9 or later)
     - pip (Python package manager)
     - poetry (Python package manager)
+
+In Binance
+    - You must have an acoount on Binance
+    - You should create your own api_key in API Manager
+    - When creating the api_key, remember to get the secret_key
+    (that's available to be copied just in the moment of creation of the api_key)
+    - After creating the api_key, you must edit it, inserting your public IP and allowing trading spot
+    (you'll find your public IP in https://whatismyipaddress.com/)
 
 Use on CMD
     - Installing Binance SDK Spot (pip install binance-sdk-spot)
@@ -18,23 +27,24 @@ Use on CMD
 from binance_common.configuration import ConfigurationRestAPI
 from binance_sdk_spot.spot import Spot
 
-my_api_key = 'ASAZ6e17Ps3J74RARr1uwCp88LWJiutEzIY4e4GSyt5391IXy5QqoXZ8ruG0jsGn'
+my_api_key = r'ASAZ6e17Ps3J74RARr1uwCp88LWJiutEzIY4e4GSyt5391IXy5QqoXZ8ruG0jsGn'
+my_secret_key = r'3pUwRYRoVrbhCcWAlcPAqr3XzYrqlmbjY86WCWmVpmnhdE5HoxlpDAXaNGk3ZSL2'
 
 config_rest_api = ConfigurationRestAPI(
-    api_key=my_api_key,     # my_api_key
-    api_secret = None,
+    api_key=my_api_key,                     # my_api_key
+    api_secret = my_secret_key,             # my_secret_key
     base_path = None,
-    timeout = 1000,
+    timeout = 1000,                         # Request timeout in milliseconds
     proxy = None,
-    keep_alive = True,
-    compression = True,
-    retries = 3,
-    backoff = 1000,
+    keep_alive = True,                      # Enable Keep-Alive
+    compression = True,                     # Enable response compression
+    retries = 3,                            # Number of retry attempts for failed requests
+    backoff = 1000,                         # Delay (ms) between retries
     https_agent = None,
     time_unit = None,
     private_key = None,
     private_key_passphrase = None,
-    custom_headers = {},
+    custom_headers = {},                    # Custom REST headers
     )
 
 client = Spot(
@@ -43,4 +53,8 @@ client = Spot(
     config_ws_streams = None,
     )
 
-account_info = client.rest_api.get_account()
+'testing'
+# account_info = client.rest_api.get_account()
+# print(account_info)
+# ticker_price = client.rest_api.get_symbol_price_ticker(symbol="BTCUSDT")
+# print(ticker_price)
