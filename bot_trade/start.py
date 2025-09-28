3b6810904c473a5b35012a27a32e5a99b350a35f
Daniel Teixeira Wanderley <dteixeirawanderley@gmail.com>
2025-09-28 02:18:56 -0300

    initiating a logger (other day I take care of this)

--- bot_trade/start.py
+++ bot_trade/start.py
@@ -44,3 +44,27 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+
+from binance_common.configuration import ConfigurationRestAPI
+from binance_common.constants import SPOT_REST_API_PROD_URL
+from binance_sdk_spot.spot import Spot
+from binance_sdk_spot.rest_api.models import ExchangeInfoResponse, RateLimits
+
+logging.basicConfig(level=logging.INFO)
+configuration = ConfigurationRestAPI(api_key="your-api-key", api_secret="your-api-secret", base_path=SPOT_REST_API_PROD_URL)
+
+client = Spot(config_rest_api=configuration)
+
+try:
+    response = client.rest_api.exchange_info(symbol="BNBUSDT")
+
+    rate_limits: List[RateLimits] = response.rate_limits
+    logging.info(f"exchange_info() rate limits: {rate_limits}")
+
+    data: ExchangeInfoResponse = response.data()
+    logging.info(f"exchange_info() response: {data}")
+except Exception as e:
+    logging.error(f"exchange_info() error: {e}")
