bc6620e3651b6409d54df6ce8bc53f9cdd8173d5
Daniel Teixeira Wanderley <dteixeirawanderley@gmail.com>
2025-10-04 00:01:53 -0300

    adding previous tax calculation and a function that checks if coin is in account also organized the code

--- bot_trade/start.py
+++ bot_trade/start.py
@@ -91,6 +91,47 @@ from websocket_streams.websocket_streams_agent import call_agg_trade
 
 from binance_sdk_spot.spot import Spot
 
+import pprint
+
+
+class MyCount:
+
+    def __init__(
+            self,
+            client
+            ):
+        self.info = rest_api_account.get_account(client=client, omit_zero_balances=True)['data']
+        self.symbol = 'BTCUSDT'
+        self.tax = self.calculate_taxes(client=client)
+    
+    def calculate_taxes(self, client) -> float:
+        '''
+        Calculate taxes rate for each order
+        '''
+        comission_data = rest_api_account.account_comission(client=client, symbol=self.symbol)['data']
+        
+        # calculate taxes
+        standard_taxes = eval(comission_data.standard_commission.taker)
+        special_taxes = eval(comission_data.special_commission.taker)
+        comission_taxes = eval(comission_data.tax_commission.taker)
+        taxes = standard_taxes + special_taxes + comission_taxes
+        
+        # calculate discount
+        discount = comission_data.discount
+        if discount.enabled_for_account:
+            asset = discount.discount_asset
+            discount_taxes = discount.discount if self.check_coin_in_account(coin=asset) else 0
+        
+        return taxes * (1-discount_taxes)
+        
+    def check_coin_in_account(self, coin: str) -> bool:
+        '''
+        Check if the coin is in account.
+        '''
+        balances = self.info.balances
+        bnb_balance = next((balance for balance in balances if coin.lower() in balance.asset.lower()), None)
+        return True if eval(bnb_balance.free) > .1 else False
+
 
 @error_handling.error_handler
 def main():
@@ -98,20 +139,15 @@ def main():
     Description:
 
     """
+
     client = Spot(
         config_rest_api = config_rest_api,
         config_ws_api = config_websocket_api,
         config_ws_streams = config_websocket_streams,
         )
     
-    result = account.call_account_comission(
-        client=client,
-        symbol="BTCUSDT",
-        id=None
-    )
-    print(result)
+    my_count = MyCount(client)
+
 
 if __name__ == "__main__":
     main()
-
-
