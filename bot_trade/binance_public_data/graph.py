4019c5177c4dd82821db0776102044bfb0434652
Daniel Teixeira Wanderley <dteixeirawanderley@gmail.com>
2025-10-10 02:06:56 -0300

    saving my studies

--- bot_trade/binance_public_data/graph.py
+++ bot_trade/binance_public_data/graph.py
@@ -1,31 +1,43 @@
 
 
 import pandas as pd
-import mplfinance as mpf
+import matplotlib.pyplot as plt
 
+def create_plot(data: pd.DataFrame, line: int):
 
-def create_plot(data: pd.DataFrame, ini: int, n_time: int):
+    start = max(line-30, 0)
+    end = line+30 if start > 0 else 60
 
-    data = data.iloc[ini:ini+n_time]  # 1 hour
-
-    data['open_time'] = pd.to_datetime(data['open_time'], unit='us')
-    data = data.set_index('open_time')
-
-    # Selecionar apenas as colunas necessárias na ordem correta
-    df_candle = data[['open', 'high', 'low', 'close', 'volume', 'ema_5_diff', 'ema_10_diff', 'ema_20_diff']]
-
-    apds = [
-        mpf.make_addplot(df_candle['ema_5_diff'], color='blue', width=2.0, linestyle='-', label='SMA 5'),
-        mpf.make_addplot(df_candle['ema_10_diff'], color='red', width=2.0, linestyle='--', label='SMA 10'),
-        mpf.make_addplot(df_candle['ema_20_diff'], color='orange', width=2.0, linestyle=':', label='SMA 20'),
-        # mpf.make_addplot(df_candle['ema_10'], color='purple', width=1.5, linestyle='-.', label='EMA 10')
-    ]
-
-    mpf.plot(df_candle, 
-         type='candle',
-         style='charles',
-         title='Candlestick - BTC/USDT com Médias',
-         ylabel='Preço (USDT)',
-         volume=True,
-         addplot=apds,
-         figratio=(12, 8))
+    df_plot = data.iloc[start:end]
+    
+    # Criar subplots
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(14, 10), 
+                                   gridspec_kw={'height_ratios': [3, 1]})
+    
+    # SUBPLOT 1 - PREÇO
+    ax1.plot(df_plot['open_time'], df_plot['mid_price'], color='blue', linewidth=2, label='Mid_price')
+    ax1.plot(df_plot['open_time'], df_plot['mid_price_rolling_7'], color='black', linewidth=2, label='Rolling Mean 7')
+    ax1.plot(df_plot['open_time'], df_plot['mid_price_ewm_3'], color='red', linewidth=2, label='EWM Mean 3')
+    # ax1.plot(df_plot['open_time'], df_plot['ohlc_avg'], color='green', linewidth=2, label='Mid_price')
+    
+    ax1.set_title(f'BTC/USDT - Preço e Volume', 
+                  fontsize=14, fontweight='bold')
+    ax1.set_ylabel('Preço (USDT)', fontsize=12)
+    ax1.legend()
+    ax1.grid(True, alpha=0.3)
+    
+    # SUBPLOT 2 - VOLUME
+    ax2.plot(df_plot['open_time'], df_plot['volume'], color='orange', linewidth=1, label='Volume')
+    ax2.bar(df_plot['open_time'], df_plot['volume_growth'], color='green', alpha=.5, label='Growth')
+    
+    ax2.set_xlabel('Data/Hora', fontsize=12)
+    ax2.set_ylabel('Volume (BTC)', fontsize=12)
+    ax2.legend()
+    ax2.grid(True, alpha=0.3)
+    
+    # Rotacionar datas
+    plt.setp(ax1.xaxis.get_majorticklabels(), rotation=45)
+    plt.setp(ax2.xaxis.get_majorticklabels(), rotation=45)
+    
+    plt.tight_layout()
+    plt.show()
--- bot_trade/binance_public_data/study.py
+++ bot_trade/binance_public_data/study.py
@@ -6,73 +6,52 @@ import glob
 import os
 from graph import create_plot
 
+columns = [
+        'open_time', 'open', 'high', 'low', 'close', 'volume',
+        'close_time', 'quote_volume', 'trades', 
+        'taker_buy_base_volume', 'taker_buy_quote_volume', 'ignore'
+    ]
 
 def main():
     """
     Description:
 
-    """
-
-    
+    """    
     this_path = os.path.dirname(__file__)
     binance_data_path = glob.glob(os.path.join(this_path, "*.csv"))    
 
-    binance_data_ex = binance_data_path[0]
+    for data in binance_data_path:
+        
+        dir_name = os.path.join(this_path, os.path.dirname(data))
+        os.makedirs(dir_name, exist_ok=True)
 
-    columns = [
-        'open_time', 'open', 'high', 'low', 'close', 'volume',
-        'close_time', 'quote_volume', 'trades', 
-        'taker_buy_base_volume', 'taker_buy_quote_volume', 'ignore'
-    ]
-    
-    # loading data as a DataFrame
-    df = pd.read_csv(binance_data_ex, names=columns)
-    df.drop(['quote_volume', 'trades'], axis=1)
-    # Treating DataFrame data
-    # df['open_time'] = df['open_time'] / 1_000_000
-    # df['open_time'] = df['open_time'] - df['open_time'].min()
-    # df['close_time'] = df['close_time'] / 1_000_000
-    # df['close_time'] = df['close_time'] - df['close_time'].min() + 1
+        # loading data as a DataFrame
+        df = pd.read_csv(data, names=columns)
+        df = df.drop(columns=['close_time', 'quote_volume', 'trades', 'taker_buy_quote_volume','ignore'])
 
-    # min_quote_volume, max_quote_volume = df['quote_volume'].min(), df['quote_volume'].max()
-    min_volume, max_volume = df['volume'].min(), df['volume'].max()
+        df['open_time'] = df['open_time']/1_000_000
+        df['open_time'] = df['open_time']-df['open_time'].min()
 
-    # print(f"Min Volume: {min_volume}")
-    # print(f"Max Volume: {max_volume}")
+        df['delta'] = df['close'] - df['open']
+        hm_dm2 = df[df['delta']>20]                 # 20 U$ candles
 
-    # Média dos 3 preços principais: High, Low, Close
-    df['typical_price'] = (df['high'] + df['low'] + df['close']) / 3
-    # Dá mais peso ao preço de fechamento
-    df['weighted_close'] = (df['high'] + df['low'] + (2 * df['close'])) / 4
-    # Média dos 4 preços do candlestick
-    df['ohlc_avg'] = (df['open'] + df['high'] + df['low'] + df['close']) / 4
+        df['mid_price'] = (df['open'] + df['high'] + df['low'] + df['close']) / 4
+        df['mid_price_rolling_7'] = df['mid_price'].rolling(window=7).mean()
+        df['mid_price_ewm_3'] = df['mid_price'].ewm(span=3, adjust=False).mean()
 
-    # Média móvel simples do preço de fechamento
-    df['sma_5'] = df['close'].rolling(window=5).mean()
-    df['sma_10'] = df['close'].rolling(window=10).mean()
-    df['sma_20'] = df['close'].rolling(window=20).mean()
+        df['volume_growth'] = df['volume'].pct_change() * 100
 
-    # Média móvel exponencial (dá mais peso aos preços recentes)
-    df['ema_5'] = df['close'].ewm(span=5, adjust=False).mean()
-    df['ema_10'] = df['close'].ewm(span=10, adjust=False).mean()
-    df['ema_20'] = df['close'].ewm(span=20, adjust=False).mean()
+        idx = hm_dm2.index
 
-    # Derivada
-    df['ema_5_diff'] = df['ema_5'].diff()
-    df['ema_10_diff'] = df['ema_10'].diff()
-    df['ema_20_diff'] = df['ema_20'].diff()
+        for i in idx:
 
-    # Média móvel do typical price
-    df['typical_price_sma'] = df['typical_price'].rolling(window=10).mean()
-    df['typical_price_ema'] = df['typical_price'].ewm(span=10, adjust=False).mean()
+            analyse_df = df.iloc[i-10:i+1]
+            print(analyse_df)
 
-    
-    print(df[df['volume']>np.mean([min_volume, max_volume])])
-    # print(df[df['quote_volume']>np.mean([min_quote_volume, max_quote_volume])])
-    create_plot(df, 51011, 40)
+            create_plot(df, i)
 
+            print('HERE')
 
-    print('HERE')
 
 
 
@@ -83,3 +62,32 @@ def main():
 if __name__ == "__main__":
     main()
 
+
+
+
+"""
+
+# Média dos 3 preços principais: High, Low, Close
+df['typical_price'] = (df['high'] + df['low'] + df['close']) / 3
+# Dá mais peso ao preço de fechamento
+df['weighted_close'] = (df['high'] + df['low'] + (2 * df['close'])) / 4
+# Média dos 4 preços do candlestick
+df['ohlc_avg'] = (df['open'] + df['high'] + df['low'] + df['close']) / 4
+
+# Média móvel simples do preço de fechamento
+df['sma_5'] = df['close'].rolling(window=5).mean()
+df['sma_10'] = df['close'].rolling(window=10).mean()
+
+# Média móvel exponencial (dá mais peso aos preços recentes)
+df['ema_5'] = df['close'].ewm(span=5, adjust=False).mean()
+df['ema_10'] = df['close'].ewm(span=10, adjust=False).mean()
+
+# Derivada
+df['ema_5_diff'] = df['ema_5'].diff()
+df['ema_10_diff'] = df['ema_10'].diff()
+
+# Média móvel do typical price
+df['typical_price_sma'] = df['typical_price'].rolling(window=10).mean()
+df['typical_price_ema'] = df['typical_price'].ewm(span=10, adjust=False).mean()
+
+"""
