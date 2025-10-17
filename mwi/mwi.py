5225eefb75902c37b8b097e045596c95d1c90b43
Daniel Teixeira Wanderley <dteixeirawanderley@gmail.com>
2025-10-17 01:56:15 -0300

    now, it's an option to download, or not, the csv file from statusinvest website also are being considered smallcaps that are almost big compannies Finally, the way how the results are shown is better and the total and partial investment are presented

--- mwi/mwi.py
+++ mwi/mwi.py
@@ -36,32 +36,38 @@ import pandas as pd
 import numpy as np
 import time
 
+# True if you want to download it
+download_csv = False
+
 _this_path = os.path.dirname(os.path.abspath(__file__))
 
-edge_options = Options()
-edge_options.add_argument("--start-maximized")
-service = Service(os.path.join(_this_path, "msedgedriver.exe"))
-driver = webdriver.Edge(service=service, options=edge_options)
+if download_csv:
+    edge_options = Options()
+    edge_options.add_argument("--start-maximized")
+    service = Service(os.path.join(_this_path, "msedgedriver.exe"))
+    driver = webdriver.Edge(service=service, options=edge_options)
 
-try:
-    driver.get("https://statusinvest.com.br/acoes/busca-avancada")
-    wait = WebDriverWait(driver, 15)
+    try:
+        driver.get("https://statusinvest.com.br/acoes/busca-avancada")
+        wait = WebDriverWait(driver, 15)
 
-    search_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="main-2"]/div[3]/div/div/div/button[2]')))
-    search_button.click()
-    time.sleep(2)
+        search_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="main-2"]/div[3]/div/div/div/button[2]')))
+        search_button.click()
+        time.sleep(2)
 
-    download_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="main-2"]/div[4]/div/div[1]/div[2]/a/i')))
-    download_button.click()
-    time.sleep(2)
-except Exception as e:
-    print(f"An error occurred: {e}")
-finally:
-    driver.quit()
+        download_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="main-2"]/div[4]/div/div[1]/div[2]/a/i')))
+        download_button.click()
+        time.sleep(2)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        driver.quit()
 
-old_csv_path = glob.glob(os.path.join(os.path.expanduser("~/Downloads"), "*statusinvest*.csv"))[0]
-new_csv_path = glob.glob(os.path.join(_this_path, "*statusinvest*.csv"))[0]
-shutil.move(old_csv_path, new_csv_path)
+    old_csv_path = glob.glob(os.path.join(os.path.expanduser("~/Downloads"), "*statusinvest*.csv"))[0]
+    new_csv_path = glob.glob(os.path.join(_this_path, "*statusinvest*.csv"))[0]
+    shutil.move(old_csv_path, new_csv_path)
+else:
+    new_csv_path = glob.glob(os.path.join(_this_path, "*statusinvest*.csv"))[0]
 
 lines = ''
 with open(new_csv_path, 'r', encoding='utf-8') as f:
@@ -90,7 +96,7 @@ df = df[df['DIV LIQ / PATRI'] < 2]
 df = df[df['ROE'] > 10]
 df = df[df['ROA'] > 10]
 df = df[df['ROIC'] > 10]
-df = df[df['LIQUIDEZ MEDIA DIARIA'] > 10**6]
+df = df[df['LIQUIDEZ MEDIA DIARIA'] > 10**5]
 
 df = df.reset_index(
     drop=True,
@@ -110,5 +116,17 @@ df['QTDE'] = np.where(
     0
 )
 df['SHARE'] = round((df['QTDE'] * df['PRECO']) / (df['QTDE'] * df['PRECO']).sum(), 2) * 100
+df['VALUE'] = df['PRECO'] * df['QTDE']
+total_investment = round(df['VALUE'].sum(), 2)
 
+print("-"*200)
+print(f"This script selected the tickers below as good investments")
+print("-"*200)
 print(df)
+
+print("-"*200)
+print(f"And these are the best options to buy")
+print(f"Total investment: R%{total_investment}")
+print("-"*200)
+
+print(df[df['OPORTUNIDADE']].reset_index(drop=True, inplace=False))
